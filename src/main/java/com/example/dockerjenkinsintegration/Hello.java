7a3be41d7944217e9cb6e3a4a7d95dab207c8368
ashikurcmt126 <ashikurcmt126@gmail.com>
2020-09-09 11:08:34 +0600

    Hello file change

--- src/main/java/com/example/dockerjenkinsintegration/Hello.java
+++ src/main/java/com/example/dockerjenkinsintegration/Hello.java
@@ -2,6 +2,6 @@ package com.example.dockerjenkinsintegration;
 
 public class Hello {
     public static void main(String args[]){
-        System.out.println("Ashikur Rahman Rashid");
+        System.out.println("Ashikur Rahman");
     }
 }
